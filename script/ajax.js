fc9f3ee73cebe9f5cf61197262410f7b523b3365
Rahman <ainurahman03@gmail.com>
2021-01-04 15:20:03 +0700

    fixing pre-load on livesearch

--- script/ajax.js
+++ script/ajax.js
@@ -3,14 +3,25 @@
 let keyword = document.getElementById('search');
 let change = document.getElementById('change');
 
-keyword.addEventListener('keypress', () => {
+keyword.addEventListener('keyup', () => {
     // buat objek ajax
     let ajax = new XMLHttpRequest();
     //cek kesiapan ajax
     ajax.onreadystatechange = () => {
         if (ajax.status == 200 && ajax.readyState == 4) {
-            console.log("gud")
             change.innerHTML = ajax.responseText;
+            progressively.init({
+                delay: 50,
+                throttle: 300,
+                smBreakpoint: 600,
+                onLoad: function(elem) {
+                    console.log(elem);
+                },
+                onLoadComplete: function() {
+                    console.log('All images have finished loading!');
+                }
+            });
+            progressively.render();
         }
 
 
